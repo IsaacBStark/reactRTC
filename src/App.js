bfe5d124130cf0c0e31d616150118cbbec55f3d2
Ike2d2 <ike2d2@thefuturesmind.com>
2023-11-13 08:57:08 -0700

    Camera Clears mediaStream when unmounted

--- src/App.js
+++ src/App.js
@@ -1,50 +1,22 @@
 import Shell from "./components/layout/Shell.jsx";
 import Navbar from "./components/layout/Navbar.jsx";
-import { useEffect, useState, useCallback } from "react";
+import { useEffect, useState, useCallback, useRef } from "react";
 
 export default function App() {
-    const [source, setSource] = useState();
-    useEffect(() => {
-        let stream;
-        async function getCam() {
-            stream = await navigator.mediaDevices.getUserMedia({
-                audio: false,
-                video: { facingMode: 'user' },
-            })
-            setSource(stream);
-        }
-        getCam();
-    }, [])
-
-    return (
-        <Shell>
-            <Navbar />
-            <div className='w-full h-full flex items-center p-64'>
-                <Video srcObject={source}></Video>
-            </div>
-        </Shell>
-    );
-}
-
-export const Video = ({ srcObject }) => {
     const [playing, setPlaying] = useState(true);
-
-    const refVideo = useCallback(
-        (video) => {
-            if (video) video.srcObject = srcObject;
-        },
-        [srcObject],
-    );
+    const [source, setSource] = useState();
 
     function handleClick(e) {
         setPlaying(!playing)
     }
 
-    if (playing) {
-        return <video ref={refVideo} autoPlay={true} onClick={(e) => handleClick(e)} className='w-96 aspect-square rounded-full object-cover overflow-hidden' />;
-    } else {
-        return <div
-            className='
+    return (
+        <Shell>
+            <Navbar />
+            <div className='w-full h-full flex items-center p-64'>
+                {playing && <Video onClick={handleClick} source={source} setSource={setSource}></Video>}
+                {!playing && <div
+                    className='
                 w-96
                 aspect-square
                 bg-black
@@ -55,6 +27,42 @@ export const Video = ({ srcObject }) => {
                 text-lg
                 font-bold
                 rounded-full'
-                onClick={(e) => handleClick(e)}>Video Muted</div>
-    }
+                    onClick={handleClick}>Video Muted</div>}
+            </div>
+        </Shell>
+    );
+}
+
+export const Video = ({ onClick, source, setSource }) => {
+    const sourceRef = useRef();
+
+    useEffect(() => {
+        sourceRef.current = source;
+    },[source])
+
+    useEffect(() => {
+        let stream;
+        async function getCam() {
+            stream = await navigator.mediaDevices.getUserMedia({
+                audio: false,
+                video: { facingMode: 'user' },
+            })
+            setSource(stream);
+        }
+        getCam();
+
+        return function cleanup() {
+            sourceRef.current.getTracks().forEach(track => {
+                track.stop();
+            });
+        }
+    }, [])
+
+    const refVideo = useCallback(
+        (video) => {
+            if (video) video.srcObject = source;
+        },
+        [source],
+    );
+    return <video ref={refVideo} autoPlay={true} onClick={onClick} className='w-96 aspect-square rounded-full object-cover overflow-hidden' />;
 };
