883a08b9cb98eaa72f0c6c8d2974bb78aafefd04
Isaac <ike2d2@thefuturesmind.com>
2023-12-04 17:29:16 -0700

    removed unused dependencies

--- src/components/Shell.jsx
+++ src/components/Shell.jsx
@@ -1,15 +1,14 @@
 import { Logo } from "./Logo"
 import { useCallInfo } from "./CallInfo"
 import { Outlet } from "react-router-dom";
-import { useEffect, useState } from "react";
 
-export function Shell({ children }) {
+export function Shell() {
     const { roomNumber, displayNumber } = useCallInfo();
 
     return (
         <div id='shell' className='w-full h-full flex flex-col p-4 relative'>
             <Logo />
-            {roomNumber && <h1 className='text-xl fixed top-2 right-2'>Room: <span className='tracking-wider'>{displayNumber}</span></h1>}
+            {displayNumber && <h1 className='text-xl fixed top-2 right-2'>Room: <span className='tracking-wider'>{displayNumber}</span></h1>}
             <Outlet />
         </div>
     )
